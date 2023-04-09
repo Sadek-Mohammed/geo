1f517bd1cbf71a5028fecdbf8f7aec452412aa18
Sadek-Mohammed <sadek.hmask@gmail.com>
2023-04-09 06:25:30 +0200

    done geo

---
+++ script.js
@@ -0,0 +1,537 @@
+let questions = [
+{
+    q:"The S-wave comes to an end at:", 
+    a:"A barrier between the crust and the mantle",
+    b:"The border between the lithosphere and the asthenosphere",
+    c:"The border between the mantle and the core",
+    d :"Core Boundaries (Inner and Outer)",
+    correct :"c"
+},
+{
+    q:"The following factors are hypothesized to be involved in plate movement:", 
+    a:"Isostasy",
+    b:"Earth’s rotation",
+    c:"Thermal convection cells",
+    d :"Polar wandering",
+    correct :"c"
+},
+{
+    q:"The element which is most widespread in the earth’s crust is:", 
+    a:"Oxygen",
+    b:"Silicon",
+    c:"Aluminium",
+    d :"Iron",
+    correct :"a"
+},
+{
+    q:"The following is the name of the instrument used to record earthquake waves:", 
+    a:"Seismograph",
+    b:"Seismogram",
+    c:"Seismometer",
+    d :"Scintillometer",
+    correct :"a"
+},
+{
+    q:"What is the source of energy for the Earth’s internal heat engine?:", 
+    a:"Solar energy",
+    b:"Ocean tides",
+    c:"Volcanoes",
+    d :"Radioactivity",
+    correct :"d"
+},
+{
+    q:"The following factors contribute to the accumulation of oxygen in the Earth’s atmosphere:", 
+    a:"Formation of the ocean in the early history of Earth",
+    b:"Algae and other organisms employed photosynthesis",
+    c:"Oxygen settled on Earth from planets further from the sun",
+    d :"Rocks weathered and released their oxygen.",
+    correct :"b"
+},
+{
+    q:"What are the cracks along with those blocks that have moved relative to one another?:", 
+    a:"Folds",
+    b:"Joints",
+    c:"Faults",
+    d :"Intrusions",
+    correct :"c"
+},
+{
+    q:"The field of geology that deals with the form, classification, mechanism, and causes of these rock structures’ evolution are known as:", 
+    a:"Rock geology",
+    b:"Structural geology",
+    c:"Basic geology",
+    d :"Lithology",
+    correct :"b"
+},
+{
+    q:"The most common rock on the planet is:", 
+    a:"Igneous rock",
+    b:"Sedimentary rock",
+    c:"Metamorphic rock",
+    d :"All are found in equal amounts.",
+    correct :"b"
+},
+{
+    q:"What are the two criteria thought to be necessary for igneous rock formation?:", 
+    a:"Low temperature and molten state",
+    b:"Molten state and very high-temperature",
+    c:"Molten state and moderate temperature",
+    d :"Crystallised state and moderate temperature",
+    correct :"b"
+},
+{
+    q:"What is the lava or magma’s current state?:", 
+    a:"Always liquid",
+    b:"Solid",
+    c:"Gaseous",
+    d :"Mixture of liquid, crystals, and gases",
+    correct :"d"
+},
+{
+    q:"Where do volcanic rocks form?:", 
+    a:"Below the surface of the earth",
+    b:"On the surface of the earth",
+    c:"Inside the cored. Between the mantle and the core",
+    d:"none of above",
+    correct :"b"
+},
+{
+    q:"Which of the following features is not associated with a convergent plate boundary?:", 
+    a:"a mid-ocean ridge",
+    b:"earthquakes",
+    c:"a deep-sea trench zone",
+    d :"volcanic activity",
+    correct :"a"
+},
+{
+    q:"The descent of oceanic lithosphere into the mantle is the process of:", 
+    a:"accretion",
+    b:"subduction",
+    c:"divergence zone",
+    d :"contraction fault",
+    correct :"b"
+},
+{
+    q:"New seafloor is created at a:", 
+    a:"deep-sea trench",
+    b:"mid-ocean ridge",
+    c:"subduction zone",
+    d :"transform fault",
+    correct :"b"
+},
+{
+    q:"Metamorphic rocks are changed rocks. Which of the following rock types could be the \"parent\" of a metamorphic rock?:", 
+    a:"sedimentary",
+    b:"igneous",
+    c:"metamorphic",
+    d :"all of the above",
+    correct :"d"
+},
+{
+    q:"Minerals:", 
+    a:"can form by life-processes -- organic",
+    b:"are crystalline solids",
+    c:"have a unique chemical composition",
+    d :"can be any state (solid, liquid, or gas) as long as that state occurs naturally",
+    correct :"b"
+},
+{
+    q:"The layer that separates crust from core is the?:", 
+    a:"magma layer",
+    b:"lithosphere",
+    c:"mantle",
+    d :"continent",
+    correct :"c"
+},
+{
+    q:"The asthenosphere is:", 
+    a:"cool and strong",
+    b:"cool and weak",
+    c:"hot and strong",
+    d :"hot and weak",
+    correct :"d"
+},
+{
+    q:"The Earth's lithosphere is broken into approximately ______ large, rigid plates.:", 
+    a:"2",
+    b:"12",
+    c:"50",
+    d :"100",
+    correct :"b"
+},
+{
+    q:"Which of the following statements about convection is true?:", 
+    a:"heat is transferred from hot material to cool material without inducing a flow.",
+    b:"cool material flows upward and displaces hot material",
+    c:"hot material flows upward and displaces cool material",
+    d :"random circulation occurs",
+    correct :"c"
+},
+{
+    q:"Approximately how fast does an Earth lithospheric plate move?:", 
+    a:"several centimeters per year",
+    b:"several centimeters per day",
+    c:"several centimeters per hour",
+    d :"several centimeters per second",
+    correct :"a"
+},
+{
+    q:"The Earth's external heat engine is not responsible for which of the following:", 
+    a:"climate",
+    b:"erosion",
+    c:"tides",
+    d :"winds",
+    correct :"c"
+},
+{
+    q:"The sediment produced by chemical weathering of granite is commonly known as:", 
+    a:"Cobble",
+    b:"Sand",
+    c:"Silt",
+    d :"Clay",
+    correct :"d"
+},
+{
+    q:"The basic and fundamental unit of Lithostratigraphic classification is:", 
+    a:"Supergroup",
+    b:"Group",
+    c:"Formation",
+    d :"Member",
+    correct :"c"
+},
+{
+    q:"The Earth’s magnetic field is produced by the interaction of what?:", 
+    a:"lithosphere and hydrosphere",
+    b:"crust and mantle",
+    c:"inner and outer core",
+    d :"outer core and mantle",
+    correct :"c"
+},
+{
+    q:"All the deep earthquakes in the world are associated with:", 
+    a:"mantle plumes",
+    b:"spreading centers",
+    c:"subduction zone",
+    d :"ocean trenches",
+    correct :"c"
+},
+{
+    q:"what type of rock makes up the greatest portion of the volume of the continental crust:", 
+    a:"Gabbro",
+    b:"Granite",
+    c:"Limestone",
+    d :"None of these",
+    correct :"b"
+},
+{
+    q:"The most necessary property of a reservoir is:", 
+    a:"Porosity",
+    b:"Permeability",
+    c:"Voids",
+    d :"None of these",
+    correct :"a"
+},
+{
+    q:"Cleavage means:", 
+    a:"development of crystal faces during mineral growth",
+    b:"Splitting a mineral along planner surfaces",
+    c:"development of irregular fractures when minerals is broken",
+    d :"density and specific gravity of minerals.",
+    correct :"b"
+},
+{
+    q:"Small pieces of rock moved from one part to other by wind and water, glacier is called as:", 
+    a:"erosion",
+    b:"rock flow",
+    c:"weathering",
+    d :"cementation",
+    correct :"c"
+},
+{
+    q:"Large inverted mushroom–shaped intrusions that sag down in the middle due to dense rocks are called as:", 
+    a:"laccoliths",
+    b:"lopoliths",
+    c:"balnoliths",
+    d :"dikes",
+    correct :"b"
+},
+{
+    q:"Type of texture formed due to rapid cooling is:", 
+    a:"aphanitic",
+    b:"phaneritic",
+    c:"course grained",
+    d :"none of these",
+    correct :"a"
+},
+{
+    q:"What is the most common structural element of group silicate mineral?:", 
+    a:"a silicon − aluminum tetrahedron",
+    b:"a silicon − nitrogen tetrahedron",
+    c:"a silicon − oxygen tetrahedron",
+    d :"a silicon − oxygen octahedron",
+    correct :"c"
+},
+{
+    q:"Diamond is having hardness number:", 
+    a:"10",
+    b:"7",
+    c:"1",
+    d :"2",
+    correct :"a"
+},
+{
+    q:"Plutonic type of rocks generally shows:", 
+    a:"fine grained texture",
+    b:"coarse grained texture",
+    c:"both b & a",
+    d :"none",
+    correct :"c"
+},
+{
+    q:"Cuprite is an ore of:", 
+    a:"Aluminum",
+    b:"Iron",
+    c:"Copper",
+    d :"Calcium",
+    correct :"c"
+},
+{
+    q:"Limestone and marble are rich in:", 
+    a:"Mica",
+    b:"Sand",
+    c:"Calcium",
+    d :"Quartz",
+    correct :"c"
+},
+{
+    q:"When an earthquake occurs, energy radiates in all directions from its source. The source is also referred to as the:", 
+    a:"inertial point",
+    b:"epicenter",
+    c:"focus",
+    d :"seismic zone",
+    correct :"c"
+},
+{
+    q:"Which one of the following statements is correct?:", 
+    a:"P waves travel through solids; S waves do not.",
+    b:"P and S waves travel through liquids, but P waves do not travel through solids.",
+    c:"S waves travel through solids and P waves travel through liquids.",
+    d :"P and S waves travel through liquids, but S waves do not travel through solids.",
+    correct :"c"
+},
+{
+    q:"________ have the highest velocities.:", 
+    a:"Primary waves",
+    b:"Secondary waves",
+    c:"Surface waves",
+    d :"Refracted S waves",
+    correct :"a"
+},
+{
+    q:"On a typical seismogram, ________ will show the highest amplitudes.", 
+    a:"P waves",
+    b:"S waves",
+    c:"surface waves",
+    d :"body waves",
+    correct :"c"
+},
+{
+    q:"An igneous rock that contains vesicles:", 
+    a:"is also extrusive",
+    b:"is also fine grained",
+    c:"contains many small holes",
+    d :"all of the above",
+    correct :"d"
+},
+{
+    q:"As the rate of cooling increases, the size of the crystals that form:", 
+    a:"increases",
+    b:"decreases",
+    c:"is not affected",
+    d :"none of these",
+    correct :"b"
+},
+{
+    q:"Obsidian exhibits a ________ texture.:", 
+    a:"fine-grained",
+    b:"glassy",
+    c:"coarse-grained",
+    d :"porphyritic",
+    correct :"b"
+},
+{
+    q:"This igneous texture is characterized by two distinctively different crystal sizes.", 
+    a:"fine-grained",
+    b:"glassy",
+    c:"coarse-grained",
+    d :"porphyritic",
+    correct :"d"
+},
+{
+    q:"Rhyolite is the fine-grained equivalent of this igneous rock.:", 
+    a:"basalt",
+    b:"andesite",
+    c:"granite",
+    d :"diorite",
+    correct :"c"
+},
+{
+    q:"The texture of an igneous rock:", 
+    a:"is controlled by the composition of magma",
+    b:"determines the color of the rock",
+    c:"is caused by leaching",
+    d :"records the rock's cooling history",
+    correct :"d"
+},
+{
+    q:"The most important mechanical weathering process is:", 
+    a:"frost action",
+    b:"hydrolysis",
+    c:"unloading",
+    d :"thermal expansion",
+    correct :"a"
+},
+{
+    q:"Chemical weathering would be most effective:", 
+    a:"in a warm, dry climate",
+    b:"in a cold, dry climate",
+    c:"in a warm, humid climate",
+    d :"equally in any kind of climate",
+    correct :"c"
+},
+{
+    q:"Which of the following is NOT a product of the chemical weathering of potassium feldspar?:", 
+    a:"silica",
+    b:"potassium ions",
+    c:"iron oxide",
+    d :"clay",
+    correct :"c"
+},
+{
+    q:"Which one of the following is NOT true about mechanical weathering?:", 
+    a:"produces smaller pieces",
+    b:"adds to the effectiveness of chemical weathering",
+    c:"does not change the rock's mineral composition",
+    d :"does not affect metamorphic rocks",
+    correct :"d"
+}
+];
+if (! "play" in localStorage)
+{
+    localStorage.play = 0;
+}
+if (! "won" in localStorage)
+{
+localStorage.won = 0;
+}
+if(! "lost" in localStorage)
+{
+    localStorage.lost = 0;
+}
+let numQuestions = 0, points = 10;
+let correct = "a";
+const displayScore = () => {
+    console.log("Hey!!");
+    document.getElementById("rn").innerText = `This Game's Score: ${points}.`;
+    document.getElementById("rec").innerText = `Played: ${localStorage.play}, Won: ${localStorage.won}, and Lost: ${localStorage.lost}`;
+    return 1; 
+}
+let questionNumbers = [];
+const newGame = () => {
+    displayScore();
+    points = 10; 
+    numQuestions = questions.length;
+    for (i = 0; i < numQuestions; i++)
+    {
+        questionNumbers.push(i);   
+    }
+}
+let guess = () => {
+    let numElements = questionNumbers.length; 
+    numElements = questionNumbers.length - 1;
+    if (numElements < 0)
+    {
+        return -1;
+    }
+    let num = Math.floor(Math.random() * (numElements - 0 + 1)) + 0;
+    num = questionNumbers[num];
+    questionNumbers.splice(questionNumbers.indexOf(num), 1);
+    return num;
+}
+const init = () => {
+    let start = document.getElementById("start-div");
+    start.insertAdjacentHTML("afterend", `<div id = "currentQ"></div>`)
+    start.remove();
+    choose();
+}
+const choose = () => {
+    
+    let currentQ = document.getElementById("currentQ");
+    let num = guess();
+    if (num == -1)
+    {
+        localStorage.play++;
+        localStorage.won++; 
+        alert("you won");
+        location.reload();
+    }
+    console.log(num);
+    let question = questions[num];
+    correct = question.correct;
+    currentQ.innerHTML = `
+    <div class = "container">
+    <h1 id = "question" >${question.q}</h1>
+    <form action="data.php" method="get" id = "questioner">
+        <div class = "option">
+        <input type="radio" name="answer" value = "a" id="a">
+        <label for="a">${question.a} </label>
+        </div>
+        <div class = "option">
+        <input type="radio" name="answer" value = "b" id="b">
+        <label for="b">${question.b} </label>
+        </div>
+        <div class = "option">
+        <input type="radio" name="answer" value = "c" id="c">
+        <label for="c">${question.c} </label>
+        </div>
+        <div class = "option">
+        <input type="radio" name="answer" value = "d" id="d">
+        <label for="d">${question.d}</label>
+        </div>
+    </form>
+    <button id="seer" onclick="see()">
+        submit
+    </button>
+    </div>
+    `
+}
+const see = () => {
+    displayScore();
+    let your = document.getElementById("questioner").answer.value;
+    console.log(your);
+    if (your === correct)
+    {
+        points += 1;
+    }
+    else if (your == "")
+    {
+        alert("you must choose answer");
+        return ;
+    }
+    else
+    {
+        points -= 1;
+    }
+    console.log(points);
+    if (points < 0)
+    {
+        localStorage.play++;
+        localStorage.lost++; 
+        alert("you lost");
+        location.reload();
+    }
+    displayScore();
+    choose();
+}
+
+newGame();
